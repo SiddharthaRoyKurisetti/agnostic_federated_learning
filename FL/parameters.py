d124f5437e9b15e3f7146ba5fe65ccef9616d655
Siddhartha Roy KURISETTI <skurisetti@umass.edu>
2024-11-29 18:31:45 -0500

    Update parameters.py

--- FL/parameters.py
+++ FL/parameters.py
@@ -25,7 +25,7 @@ def get_args():
     parser.add_argument('--federated_type',type=str, default='fedavg',choices=['fedavg','afl'])
     parser.add_argument('--model',type=str, default='cnn',choices=['cnn','mlp'])
     parser.add_argument('--n_clients',type=int, default=3, help='the number of clients')
-    parser.add_argument('--global_epochs',type=int, default=30, help='the number of global epochs')
+    parser.add_argument('--global_epochs',type=int, default=10, help='the number of global epochs')
     parser.add_argument('--local_epochs',type=int, default=5, help='the number of local epochs')
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument("--on_cuda",  default="yes", type=strtobool)
